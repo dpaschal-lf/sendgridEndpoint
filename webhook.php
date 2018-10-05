76ba48fbefa2c99a43381f7e306c09cafd8ecc1f
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-10-04 18:22:01 -0700

    added logging

--- webhook.php
+++ webhook.php
@@ -2,6 +2,8 @@
 
 require_once('credentials.php');
 
+error_log("something wrong, ".print_r($conn, true));
+
 $query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 mysqli_query($conn, $query);
