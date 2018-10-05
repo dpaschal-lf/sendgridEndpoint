c931e5e6fac9193537f6669d282d7e4ac789f7fc
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-10-04 18:25:11 -0700

    checking for post

--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 
 require_once('credentials.php');
 
-error_log("something wrong, ".print_r($conn, true));
+error_log("something wrong POST, ".print_r($_POST, true));
 
 $query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
