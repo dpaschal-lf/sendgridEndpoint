e0d716a74b07ee4dd005f30e760e929ee1eb5bdb
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-10-04 18:28:27 -0700

    added check for html body

--- webhook.php
+++ webhook.php
@@ -2,9 +2,9 @@
 
 require_once('credentials.php');
 
-error_log("something wrong POST, ".print_r($_POST, true));
+//error_log("something wrong POST, ".print_r($_POST, true));
 
-$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 mysqli_query($conn, $query);
 
